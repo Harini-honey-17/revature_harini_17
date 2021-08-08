e76976f12607db7cad916b9326e49c55f6eaa1a3
91949 <harini.yenumala@gmai;.com>
2021-08-09 00:16:47 +0530

    aug5

---
+++ aug5/src/task2/ItemList.java
@@ -0,0 +1,14 @@
+package task2;
+
+public class ItemList {
+
+	public ItemList(String string, int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void printStatement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
